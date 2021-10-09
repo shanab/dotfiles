27ce14376664b7e666a1fc9edef495a141437bc3
Ahmed Aboshanab <shanab@hey.com>
2021-10-09 16:42:50 +0200

    Add jsonls and yamlls with basic schema detection

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -396,38 +396,110 @@ capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
 
 -- Enable the following language servers
 local servers = {
-	"clangd",
-	"rust_analyzer",
-	"pyright",
-	"tsserver",
-}
-for _, lsp in ipairs(servers) do
-	nvim_lsp[lsp].setup({
-		on_attach = on_attach,
-		capabilities = capabilities,
-	})
-end
-
--- Try out gopls config
-nvim_lsp["gopls"].setup({
-	on_attach = on_attach,
-	capabilities = capabilities,
-	cmd = {
-		"gopls",
-		"serve",
-	},
-	root_dir = function(fname)
-		return nvim_lsp.util.root_pattern("go.mod", ".git")(fname) or vim.loop.cwd()
-	end,
-	settings = {
-		gopls = {
-			analyses = {
-				unusedparams = true,
+	clangd = {},
+	rust_analyzer = {},
+	pyright = {},
+	tsserver = {},
+	gopls = {
+		cmd = {
+			"gopls",
+			"serve",
+		},
+		root_dir = function(fname)
+			return nvim_lsp.util.root_pattern("go.mod", ".git")(fname) or vim.loop.cwd()
+		end,
+		settings = {
+			gopls = {
+				analyses = {
+					unusedparams = true,
+				},
+				staticcheck = true,
 			},
-			staticcheck = true,
 		},
 	},
-})
+	jsonls = {
+		filetypes = { "json", "jsonc" },
+		settings = {
+			json = {
+				-- Schemas https://www.schemastore.org
+				schemas = {
+					{
+						fileMatch = { "package.json" },
+						url = "https://json.schemastore.org/package.json",
+					},
+					{
+						fileMatch = { "tsconfig*.json" },
+						url = "https://json.schemastore.org/tsconfig.json",
+					},
+					{
+						fileMatch = {
+							".prettierrc",
+							".prettierrc.json",
+							"prettier.config.json",
+						},
+						url = "https://json.schemastore.org/prettierrc.json",
+					},
+					{
+						fileMatch = { ".eslintrc", ".eslintrc.json" },
+						url = "https://json.schemastore.org/eslintrc.json",
+					},
+					{
+						fileMatch = { ".babelrc", ".babelrc.json", "babel.config.json" },
+						url = "https://json.schemastore.org/babelrc.json",
+					},
+					{
+						fileMatch = { "lerna.json" },
+						url = "https://json.schemastore.org/lerna.json",
+					},
+					{
+						fileMatch = { "now.json", "vercel.json" },
+						url = "https://json.schemastore.org/now.json",
+					},
+					{
+						fileMatch = {
+							".stylelintrc",
+							".stylelintrc.json",
+							"stylelint.config.json",
+						},
+						url = "http://json.schemastore.org/stylelintrc.json",
+					},
+				},
+			},
+		},
+	},
+	yamlls = {
+		settings = {
+			yaml = {
+				-- Schemas https://www.schemastore.org
+				schemas = {
+					["http://json.schemastore.org/gitlab-ci.json"] = { ".gitlab-ci.yml" },
+					["https://json.schemastore.org/bamboo-spec.json"] = {
+						"bamboo-specs/*.{yml,yaml}",
+					},
+					["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] = {
+						"docker-compose*.{yml,yaml}",
+					},
+					["http://json.schemastore.org/github-workflow.json"] = ".github/workflows/*.{yml,yaml}",
+					["http://json.schemastore.org/github-action.json"] = ".github/action.{yml,yaml}",
+					["http://json.schemastore.org/prettierrc.json"] = ".prettierrc.{yml,yaml}",
+					["http://json.schemastore.org/stylelintrc.json"] = ".stylelintrc.{yml,yaml}",
+					["http://json.schemastore.org/circleciconfig"] = ".circleci/**/*.{yml,yaml}",
+				},
+			},
+		},
+	},
+}
+
+for server, config in pairs(servers) do
+	local server_disabled = (config.disabled ~= nil and config.disabled) or false
+
+	if not server_disabled then
+		nvim_lsp[server].setup(vim.tbl_deep_extend("force", {
+			on_attach = on_attach,
+			capabilities = capabilities,
+		}, config))
+	end
+end
 
 local sumneko_root_path = vim.fn.getenv("HOME") .. "/.local/bin/lua-language-server"
 local sumneko_binary = sumneko_root_path .. "/bin/macOS/lua-language-server"
